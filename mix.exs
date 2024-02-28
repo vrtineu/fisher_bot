80b6800833ae228a1fc8e3d9a18ae94344d83380
vrtineu <vininro@gmail.com>
2024-02-28 20:19:26 -0300

    Add nosedrum dependency

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule Fisher.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
 
       {:nostrum, "~> 0.8"},
-      {:dotenvy, "~> 0.8.0"}
+      {:dotenvy, "~> 0.8.0"},
+      {:nosedrum, "~> 0.5"}
     ]
   end
 
