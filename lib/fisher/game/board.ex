27b8684dcd763d3878611eede42986c2b44e46f8
vrtineu <vininro@gmail.com>
2024-02-27 21:30:27 -0300

    Refactor board initialization and element placement

--- lib/fisher/game/board.ex
+++ lib/fisher/game/board.ex
@@ -25,11 +25,11 @@ defmodule Fisher.Game.Board do
   def new(xy_size, opts \\ [])
 
   def new({x_size, y_size} = xy_size, opts) when x_size > 0 and y_size > 0 do
-    board = matrix(xy_size)
+    matrix = matrix(xy_size)
 
     case Keyword.get(opts, :elements, false) do
-      true -> {:ok, put_elements(board)}
-      false -> {:ok, board}
+      true -> {:ok, put_elements(matrix)}
+      false -> {:ok, matrix}
     end
   end
 
@@ -49,33 +49,25 @@ defmodule Fisher.Game.Board do
     |> place_player_rod(random_position())
   end
 
-  defp random_position() do
-    {Enum.random(0..4), Enum.random(0..4)}
+  defp random_position, do: {Enum.random(0..4), Enum.random(0..4)}
+
+  defp place_fish(matrix, {x, y}) do
+    place_on_matrix(matrix, {x, y}, :fish)
   end
 
-  defp place_fish(board, {x, y}) do
-    place_on_board(board, {x, y}, :fish)
+  defp place_player_rod(matrix, {x, y}) do
+    place_on_matrix(matrix, {x, y}, :fishing_rod)
   end
 
-  defp place_player_rod(board, {x, y}) do
-    place_on_board(board, {x, y}, :fishing_rod)
-  end
-
-  defp place_on_board(board, {x, y}, element) do
-    Enum.with_index(board, fn row, row_index ->
+  defp place_on_matrix(matrix, {x, y}, element) do
+    Enum.with_index(matrix, fn row, row_index ->
       Enum.with_index(row, fn cell, cell_index ->
-        place_on_board({row_index, cell_index}, {x, y}, element, cell)
+        if row_index == x and cell_index == y do
+          element
+        else
+          cell
+        end
       end)
     end)
-  end
-
-  defp place_on_board({current_x, current_y}, {x, y}, element, cell) do
-    case cell do
-      :water when current_x == x and current_y == y ->
-        element
-
-      _ ->
-        cell
-    end
   end
 end
